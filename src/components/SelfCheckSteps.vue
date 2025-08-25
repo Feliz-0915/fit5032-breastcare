a7c68a2c2b271211661f3753bc4ae4fe892c7395
Tianyang <thuu0017@student.monash.edu>
2025-08-26 00:48:50 +1000

    feat(b2): render self-check steps from JSON

---
+++ src/components/SelfCheckSteps.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import stepsJson from '../data/self-check-steps.json'
+
+const steps = ref([])
+onMounted(() => {
+  steps.value = stepsJson
+})
+</script>
+
+<template>
+  <div class="row g-3">
+    <div class="col-12 col-md-6 col-xl-4" v-for="s in steps" :key="s.id">
+      <div class="card h-100">
+        <img
+          v-if="s.img"
+          :src="s.img"
+          :alt="s.title"
+          class="card-img-top img-fluid"
+          loading="lazy"
+        />
+        <div class="card-body">
+          <h5 class="card-title">{{ s.title }}</h5>
+          <p class="card-text text-muted">{{ s.desc }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/pages/Selfcheck.vue
@@ -0,0 +1,10 @@
+<template>
+  <section>
+    <h2 class="h4 mb-3">Self-check Guide</h2>
+    <SelfCheckSteps />
+  </section>
+</template>
+
+<script setup>
+import SelfCheckSteps from '../components/SelfCheckSteps.vue'
+</script>
