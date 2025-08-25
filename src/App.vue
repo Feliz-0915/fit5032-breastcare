e9e8f273561dd8ce4e3d2f743d6d15a51b7cb5b5
Tianyang <thuu0017@student.monash.edu>
2025-08-25 16:52:23 +1000

    feat(ui): add App shell and responsive navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div>
+    <NavBar />
+    <main class="py-4">
+      <div class="container">
+        <router-view />
+      </div>
+    </main>
+  </div>
 </template>
 
-<style scoped></style>
+<script setup>
+import NavBar from './components/NavBar.vue'
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="navbar navbar-expand-md navbar-dark bg-primary">
+    <div class="container">
+      <RouterLink class="navbar-brand" to="/">BreastCare</RouterLink>
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#mainNav"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div id="mainNav" class="collapse navbar-collapse">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item"><RouterLink class="nav-link" to="/">Home</RouterLink></li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/selfcheck">Self Check</RouterLink>
+          </li>
+          <li class="nav-item"><RouterLink class="nav-link" to="/finder">Finder</RouterLink></li>
+          <li class="nav-item"><RouterLink class="nav-link" to="/form">Form</RouterLink></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
